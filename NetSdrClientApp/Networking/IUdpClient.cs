d03b18dcca8556611c0af11179f52bd47d4d44c3
SadRboy <gimnazia.165@gmail.com>
2025-10-20 19:21:52 +0300

    Lab_4

--- NetSdrClientApp/Networking/IUdpClient.cs
+++ NetSdrClientApp/Networking/IUdpClient.cs
@@ -6,5 +6,5 @@ public interface IUdpClient
     Task StartListeningAsync();
 
     void StopListening();
-    void Exit();
+    
 }
--- NetSdrClientApp/Networking/TcpClientWrapper.cs
+++ NetSdrClientApp/Networking/TcpClientWrapper.cs
@@ -73,30 +73,17 @@ namespace NetSdrClientApp.Networking
 
         public async Task SendMessageAsync(byte[] data)
         {
-            if (Connected && _stream != null && _stream.CanWrite)
-            {
-                Console.WriteLine($"Message sent: " + data.Select(b => Convert.ToString(b, toBase: 16)).Aggregate((l, r) => $"{l} {r}"));
-                await _stream.WriteAsync(data, 0, data.Length);
-            }
-            else
-            {
+            if (!Connected || _stream == null || !_stream.CanWrite)
                 throw new InvalidOperationException("Not connected to a server.");
-            }
+
+            Console.WriteLine("Message sent: " +
+                              string.Join(" ", data.Select(b => b.ToString("X2"))));
+
+            await _stream.WriteAsync(data, 0, data.Length);
         }
 
         public async Task SendMessageAsync(string str)
-        {
-            var data = Encoding.UTF8.GetBytes(str);
-            if (Connected && _stream != null && _stream.CanWrite)
-            {
-                Console.WriteLine($"Message sent: " + data.Select(b => Convert.ToString(b, toBase: 16)).Aggregate((l, r) => $"{l} {r}"));
-                await _stream.WriteAsync(data, 0, data.Length);
-            }
-            else
-            {
-                throw new InvalidOperationException("Not connected to a server.");
-            }
-        }
+            => await SendMessageAsync(Encoding.UTF8.GetBytes(str));
 
         private async Task StartListeningAsync()
         {
